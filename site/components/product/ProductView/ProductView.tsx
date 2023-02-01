b077d07cdb8ebcb0fd13d5f3a1dc723e65da7740
byeongdolee <bdbdbdlee@gmail.com>
2023-02-01 23:01:34 +0900

    init

--- site/components/product/ProductView/ProductView.tsx
+++ site/components/product/ProductView/ProductView.tsx
@@ -24,7 +24,15 @@ const ProductView: FC<ProductViewProps> = ({ product, relatedProducts }) => {
 
   return (
     <>
-      <Container className="max-w-none w-full" clean>
+      <button
+        onClick={(e) => {
+          e.preventDefault()
+          alert('hello world')
+        }}
+      >
+        CLICK ME
+      </button>
+      <Container className="w-full max-w-none" clean>
         <div className={cn(s.root, 'fit')}>
           <div className={cn(s.main, 'fit')}>
             <ProductTag
@@ -65,11 +73,11 @@ const ProductView: FC<ProductViewProps> = ({ product, relatedProducts }) => {
           />
         </div>
         <hr className="mt-7 border-accent-2" />
-        <section className="py-12 px-6 mb-10">
+        <section className="px-6 py-12 mb-10">
           <Text variant="sectionHeading">Related Products</Text>
           <div className={s.relatedProductsGrid}>
             {relatedProducts.map((p) => (
-              <div key={p.path} className="bg-accent-0 border border-accent-2">
+              <div key={p.path} className="border bg-accent-0 border-accent-2">
                 <ProductCard
                   noNameTag
                   product={p}
